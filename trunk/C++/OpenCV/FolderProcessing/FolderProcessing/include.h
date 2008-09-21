3418d3d72dc9e579a672ae90aa26ac20606d392d
dangvinhbao@gmail.com <dangvinhbao@gmail.com@21bdbfa0-87e6-11dd-9299-37fd5bf03096>
2008-09-21 16:51:39 +0000

    + Add some source images to test the program. + Add include.h to manage all header files. ! Finish two main features: delete an existed directory and process all image files in an directory.

---
+++ trunk/C++/OpenCV/FolderProcessing/FolderProcessing/include.h
@@ -0,0 +1,14 @@
+#ifndef _HEADER_FILES
+#define _HEADER_FILES
+
+#include <shlwapi.h>
+#include <stdio.h>
+#include <io.h>
+#include <sys/stat.h>
+#include <windows.h>
+
+#include <string>
+
+using namespace std;
+
+#endif
