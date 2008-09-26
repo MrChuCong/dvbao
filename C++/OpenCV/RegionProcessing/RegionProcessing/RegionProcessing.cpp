885b28941c6d92c508cdb7f34f9d17ec389302e0
dangvinhbao@gmail.com <dangvinhbao@gmail.com@21bdbfa0-87e6-11dd-9299-37fd5bf03096>
2008-09-26 13:54:27 +0000

    ! Import RegionProcessing project. This is an OpenCV task. User can use mouse to select a region on an image, and only the region will be applied by a simple filter.

---
+++ C++/OpenCV/RegionProcessing/RegionProcessing/RegionProcessing.cpp
@@ -0,0 +1,84 @@
+#include <cv.h>
+#include <cxcore.h>
+#include <highgui.h>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+#define WINDOW_ID "Region Processing"
+
+bool dragging = false;
+CvPoint startPoint;
+CvPoint endPoint;
+IplImage* originalImage;
+IplImage* currentImage;
+
+void FilteringImage(IplImage* image, CvPoint startPoint, CvPoint endPoint)
+{
+	int x1 = min(startPoint.x, endPoint.x);
+	int x2 = max(startPoint.x, endPoint.x);
+	int y1 = min(startPoint.y, endPoint.y);
+	int y2 = max(startPoint.y, endPoint.y);
+	unsigned char* imageData = (unsigned char*)image->imageData;
+	for (int i=y1; i<=y2; i++)
+	{
+		for (int j=x1; j<=x2; j++)
+		{
+			unsigned char* pointer = imageData + i * image->widthStep + j * image->nChannels;
+			unsigned char intensity = (pointer[0] + pointer[1] + pointer[2]) / 3;
+			pointer[0] = pointer[1] = pointer[2] = intensity;
+		}
+	}
+}
+
+void mouseHandler (int mouseEvent, int x, int y, int flags, void* param)
+{
+	switch (mouseEvent)
+	{
+	case CV_EVENT_LBUTTONDOWN:
+		if (!dragging)
+		{
+			startPoint = cvPoint(x, y);
+			dragging = true;
+		}
+		break;
+	case CV_EVENT_LBUTTONUP:
+		if (dragging)
+		{
+
+			dragging = false;
+		}
+		break;
+	case CV_EVENT_MOUSEMOVE:
+		if (dragging)
+		{
+			endPoint = cvPoint(x, y);
+			if (!currentImage)
+			{
+				cvReleaseImage(&currentImage);
+			}
+			currentImage = cvCloneImage(originalImage);
+			FilteringImage(currentImage, startPoint, endPoint);
+			cvRectangle(currentImage, startPoint, endPoint, cvScalar(0, 0, 0), 2);
+			cvShowImage(WINDOW_ID, currentImage);
+		}
+		break;
+	}
+}
+
+int main (int argc, char* argv[])
+{
+	originalImage = cvLoadImage("input.jpg");
+	cvNamedWindow(WINDOW_ID);
+	cvMoveWindow(WINDOW_ID, 100, 100);
+	cvSetMouseCallback(WINDOW_ID, mouseHandler, 0);
+	cvShowImage(WINDOW_ID, originalImage);
+	cvWaitKey(0);
+	if (!currentImage)
+	{
+		cvReleaseImage(&currentImage);
+	}
+	cvReleaseImage(&originalImage);
+	return 0;
+}
